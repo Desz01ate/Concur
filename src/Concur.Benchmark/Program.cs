4d7abe8f00fdb7effdab913b7f60f761ca41edd8
Deszolate_C <deszolate@icloud.com>
2025-07-08 22:34:13 +0700

    Feat/benchmark (#8)
    * Add benchmarking project with initial benchmarks for goroutines and channel implementations
    
    * Refactor benchmarks: update iteration/concurrency settings, add baseline attribute, simplify sum calculations, and enhance README with performance considerations.
    
    * Added benchmark to ensure the performance characteristic.
    
    * Lower target framework for CI compatibility

---
+++ src/Concur.Benchmark/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Threading.Channels;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+using Concur;
+using Concur.Implementations;
+using static Concur.ConcurRoutine;
+
+[MemoryDiagnoser]
+[ThreadingDiagnoser]
+[DisassemblyDiagnoser(printSource: true, maxDepth: 2)]
+[IterationCount(100)]
+[WarmupCount(100)]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[MinColumn, MaxColumn, MeanColumn, MedianColumn]
+public class GoBenchmark
+{
+    private const int Iterations = 1_000_000;
+    private const int Concurrency = 16;
+    private const int ExpectedSum = Iterations * Concurrency;
+
+    [Benchmark]
+    public async Task Goroutine_WithWaitGroup()
+    {
+        var wg = new WaitGroup();
+        var channel = new DefaultChannel<int>();
+
+        for (var i = 0; i < Concurrency; i++)
+        {
+            _ = Go(wg, async ch =>
+            {
+                for (var j = 0; j < Iterations; j++)
+                {
+                    await ch.WriteAsync(1);
+                }
+            }, channel);
+        }
+
+        _ = Go(async () =>
+        {
+            await wg.WaitAsync();
+            await channel.CompleteAsync();
+        });
+
+        var sum = await channel.SumAsync();
+
+        if (sum != ExpectedSum)
+        {
+            throw new Exception("Sum is not correct");
+        }
+    }
+
+    [Benchmark(Baseline = true)]
+    public async Task Channel_WithTpl()
+    {
+        var channel = Channel.CreateUnbounded<int>();
+        var writer = channel.Writer;
+        var reader = channel.Reader;
+
+        var producerTasks = new List<Task>();
+
+        for (var i = 0; i < Concurrency; i++)
+        {
+            producerTasks.Add(Task.Run(async () =>
+            {
+                for (var j = 0; j < Iterations; j++)
+                {
+                    await writer.WriteAsync(1);
+                }
+            }));
+        }
+
+        var completionTask = Task.Run(async () =>
+        {
+            await Task.WhenAll(producerTasks);
+            writer.Complete();
+        });
+
+        var sum = await reader.ReadAllAsync().SumAsync();
+
+        await completionTask;
+
+        if (sum != ExpectedSum)
+        {
+            throw new Exception("Sum is not correct");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<GoBenchmark>();
+    }
+}
